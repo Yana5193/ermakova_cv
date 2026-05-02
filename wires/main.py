6a2592f39c24c2e38aca3d655ecd14fa3375f6a1
Yana <anaermakova948@gmail.com>
2026-05-02 15:01:02 +0800

    wires

--- wires/main.py
+++ wires/main.py
@@ -1,11 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from skimage.measure import label
-from skimage.morphology import binary_opening
+from skimage.morphology import opening
 
 image=np.load("C:/Users/anaer/OneDrive/Desktop/учеба/ИГУ/computer_vision/wires/wires3.npy")
 struct=np.ones((3,1))
-process=binary_opening(image,struct)
+process=opening(image,struct)
 
 labeled_image=label(image)
 labeled_process=label(process)
@@ -14,7 +14,7 @@ print(f"Processed {np.max(labeled_process)}")
 num_wires = np.max(labeled_image)
 for wire_num in range(1, num_wires + 1):
     current = (labeled_image == wire_num)
-    n=binary_opening(current,struct)
+    n=opening(current,struct)
     parts = np.max(label(n))
     print(f"Провод {wire_num} порван на {parts} частей")
 plt.subplot(121)
